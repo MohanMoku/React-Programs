f9a12e46b550468953b949428cc144908ef8c663
Mohan Kumar S <mohanmoku2004@gamil.com>
2025-04-10 20:49:18 +0530

    9thExp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
-// import Prgm1 from './component/Exp1/Prgm1'
-// import Prgm2 from './component/Exp2/Prgm2'
-// import Prgm3 from './component/Exp3/Prgm3'
-// import Prgm4 from './component/Exp4/Prgm4'
-// import Prgm5 from './component/Exp5/Prgm5'
-// import Prgm6 from './component/Exp6/Prgm6'
-// import Prgm7 from './component/Exp7/Prgm7'
+import Prgm1 from './component/Exp1/Prgm1'
+import Prgm2 from './component/Exp2/Prgm2'
+import Prgm3 from './component/Exp3/Prgm3'
+import Prgm4 from './component/Exp4/Prgm4'
+import Prgm5 from './component/Exp5/Prgm5'
+import Prgm6 from './component/Exp6/Prgm6'
+import Prgm7 from './component/Exp7/Prgm7'
 import Prgm8 from './component/Exp8/Prgm8'
 
 function App() {
   return (
     <div className="App">
-      {/* <Prgm1 />
+      <Prgm1 />
       <Prgm2 />
       <Prgm3 />
       <Prgm4 />
       <Prgm5 />
       <Prgm6 />
-      <Prgm7 /> */}
+      <Prgm7 />
       <Prgm8 />
     </div>
   );
